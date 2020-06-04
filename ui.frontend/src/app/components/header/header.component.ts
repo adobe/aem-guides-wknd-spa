734922240f53b293cf8957a4e98bc1cb47de1469
Daniel Gordon <dgordon@adobe.com>
2020-06-04 15:55:37 -0700

    updates to starter chapter

--- ui.frontend/src/app/components/header/header.component.ts
+++ ui.frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  isMenuOpen = false;
+  isHome = false;
+  homePageUrl;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
+    this.setHomePage();
+  }
+
+
+  setHomePage() {
+    this.isHome = '/content/wknd-spa-angular/us/en/home' === this.route.snapshot.data.path;
+    this.homePageUrl = '/content/wknd-spa-angular/us/en/home.html';
+  }
+
+  onBackClick() {
+  this.location.back();
+  }
+
+  onMenuToggle() {
+    this.isMenuOpen = this.isMenuOpen ? false : true;
   }
 
 }
