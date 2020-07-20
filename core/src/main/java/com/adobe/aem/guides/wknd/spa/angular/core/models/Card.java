e42f1a8e590a6a7c581df82119283cca126b9ee6
Daniel Gordon <dgordon@adobe.com>
2020-07-20 16:25:28 -0700

    chapter solution

--- core/src/main/java/com/adobe/aem/guides/wknd/spa/angular/core/models/Card.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/spa/angular/core/models/Card.java
@@ -8,4 +8,33 @@ import org.osgi.annotation.versioning.ProviderType;
 @ProviderType
 public interface Card extends Image {
 
+    /***
+     * The URL to populate the CTA button as part of the card.
+     * The link should be based on the cardPath property that points to a page.
+     * @return String URL
+     */
+    public String getCtaLinkURL();
+
+    /***
+     * The text to display on the CTA button of the card.
+     * @return String CTA text
+     */
+    public String getCtaText();
+
+
+
+    /***
+     * The date to be displayed as part of the card. 
+     * This is based on the last modified date of the page specified by the cardPath
+     * @return 
+     */
+    public Calendar getCardLastModified();
+
+
+    /**
+     * Return the title of the page specified by cardPath if `titleFromPage` is set to true.
+     * Otherwise return the value of `cardTitle` 
+     * @return
+     */
+    public String getCardTitle();
 }
--- ui.frontend/src/app/components/card/card.component.ts
+++ ui.frontend/src/app/components/card/card.component.ts
@@ -17,11 +17,28 @@ export class CardComponent implements OnInit {
   @Input() src: string;
   @Input() alt: string;
   @Input() title: string;
+  @Input() cardTitle: string;
+  @Input() cardLastModified: number;
+  @Input() ctaLinkURL: string;
+  @Input() ctaText: string;
 
   constructor() { }
 
   get hasContent(): boolean {
     return this.src && this.src.trim().length > 0;
+  }
+
+  get hasCTA(): boolean {
+    return this.ctaLinkURL && this.ctaLinkURL.trim().length > 0 && this.ctaText && this.ctaText.trim().length > 0;
+  }
+
+  get lastModifiedDate(): string {
+    const lastModifiedDate = this.cardLastModified ? new Date(this.cardLastModified) : null;
+
+    if (lastModifiedDate) {
+      return lastModifiedDate.toLocaleDateString();
+    }
+    return null;
   }
 
   ngOnInit(): void {
