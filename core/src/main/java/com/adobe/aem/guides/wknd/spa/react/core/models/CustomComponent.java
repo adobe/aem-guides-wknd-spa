da1735116dfa6e46eb913abe68393953a980f515
Amy Merrill <amy.merrill@phoenix.edu>
2020-08-13 15:55:56 -0700

    yep

--- core/src/main/java/com/adobe/aem/guides/wknd/spa/react/core/models/CustomComponent.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/spa/react/core/models/CustomComponent.java
@@ -1,6 +1,7 @@
 package com.adobe.aem.guides.wknd.spa.react.core.models;
+import com.adobe.cq.export.json.ComponentExporter;
 
-public interface CustomComponent {
+public interface CustomComponent extends ComponentExporter {
 
     public String getMessage();
 
--- core/src/main/java/com/adobe/aem/guides/wknd/spa/react/core/models/impl/CustomComponentImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/spa/react/core/models/impl/CustomComponentImpl.java
@@ -4,19 +4,20 @@ import org.apache.sling.models.annotations.*;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import com.adobe.aem.guides.wknd.spa.react.core.models.CustomComponent;
-import org.apache.commons.lang3.StringUtils;
 
 @Model(adaptables = SlingHttpServletRequest.class, adapters = { CustomComponent.class,
     ComponentExporter.class }, resourceType = CustomComponentImpl.RESOURCE_TYPE, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class CustomComponentImpl implements CustomComponent {
 
-    static final String RESOURCE_TYPE = "wknd-spa-react/components/custom-component";
-
     @ValueMapValue
     private String message;
+
+    static final String RESOURCE_TYPE = "wknd-spa-react/components/custom-component";
 
     @Override
     public String getMessage() {
@@ -27,5 +28,6 @@ public class CustomComponentImpl implements CustomComponent {
     public String getExportedType() {
         return CustomComponentImpl.RESOURCE_TYPE;
     }
+
 
 } 
