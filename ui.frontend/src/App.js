48e816a68570c83f7be0b81eb5093fdc63de3a0f
david g <davidjgonzalez@gmail.com>
2022-08-16 12:50:05 -0400

    Removed unnecessary EditableComponent wrapper

--- ui.frontend/src/App.js
+++ ui.frontend/src/App.js
@@ -1,12 +1,16 @@
-import { EditableComponent, Page } from '@adobe/aem-react-editable-components';
+import { Page } from '@adobe/aem-react-editable-components';
 import React from 'react';
 
-// This component is the application entry point
-
+/**
+ * This component is the application entry point.
+ * 
+ * Optionally the index.js's use of <App ../> can be replaced with <Page ../>
+ * 
+ * We'll keep this broken out in case we need to do more complex App-level configurations
+ * like setting context, etc.
+ */
 const App = (props) => (
-    <EditableComponent {...props}>
-        <Page {...props}/>
-    </EditableComponent>
+    <Page {...props}/>
 )
 
 export default App;
